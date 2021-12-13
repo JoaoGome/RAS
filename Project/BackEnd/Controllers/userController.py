cd4cada18e5843240f3a822f8093c394878c8b5d
João Gomes <johny@Joaos-MBP.lan>
2021-12-13 16:18:16 +0000

    Controler e rotas do user

--- Project/BackEnd/Controllers/userController.py
+++ Project/BackEnd/Controllers/userController.py
@@ -17,22 +17,52 @@ def create_server_connection(host_name, user_name, user_password,db_name):
 
     return connection
 
-# executa a query @query 
+# usar esta função quando for para executar uma query e só queremos saber se correu bem (200) ou mal (400)
 def execute_query(connection, query):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
         connection.commit()
-        return (200)
+        return 200
     except Error as err:
-        return (400)
+        return 400
+
+# usar esta função quando for para executar uma query e receber um resultado. Neste caso, o resultado vai ser devolvido na variavel result
+def read_query(query):
+    connection = create_server_connection("localhost", "root", "JprG7654", "RAS")
+    cursor = connection.cursor()
+    result = None
+    try:
+        cursor.execute(query)
+        result = cursor.fetchall()
+        return result
+    except Error as err:
+        return 400
 
 
+# função a usar para registar um user
 def registerUser(name, password, amount, isAdmin):
     connection = create_server_connection("localhost", "root", "JprG7654", "RAS")
     query = f'''
-        INSERT INTO user VALUES
-        (4, '{name}', '{password}', {amount}, {isAdmin});
+        INSERT INTO user (name, password, amount, isAdmin) 
+        VALUES  ('{name}', '{password}', {amount}, {isAdmin});
     '''
     code = execute_query(connection,query)
     return code
+
+# função a usar para receber a lista de todos os users e os seus dados
+def getUsers():
+    connection = create_server_connection("localhost", "root", "JprG7654", "RAS")
+    query = f'''
+        SELECT * FROM user;
+    '''
+    return read_query(query)
+
+# função a usar para receber os dados de um user dado o ser name
+def getUser(name):
+    connection = create_server_connection("localhost", "root", "JprG7654", "RAS")
+    query = f'''
+        SELECT * FROM user WHERE user.name = "{name}";
+    '''
+    return read_query(query)
+    
--- Project/BackEnd/db.py
+++ Project/BackEnd/db.py
@@ -2,11 +2,44 @@ from flask import Flask, render_template, request, redirect
 import Controllers.userController as userController
 
 app = Flask(__name__)
+# rota usada para devolver a lista de todos os users e os seus dados
+# devolve em formato json/dict 
+@app.route('/users')
+def users():
+    lista = userController.getUsers()
+    result = {}
+    for l in lista:
+        tmp = {l[0]: {
+                "name": l[1],
+                "password": l[2],
+                "amount": l[3],
+                "isAdmin": l[4]
+              }}
+        result.update(tmp)
 
+    return result
 
+# rota a usar para receber os dados de um user em particular, definido pelo name (username)
+# devolve o resultado em json/dict
+@app.route('/user/<user_name>')
+def user(user_name):
+    user = userController.getUser(user_name)
+    if not user:
+        return {}
 
+    return {
+            "user_id": user[0][0],
+            "name": user[0][1],
+            "password": user[0][2],
+            "amount": user[0][3],
+            "isAdmin": user[0][4]
+           }
+    
+
+# rota usada para registar um user. 
+# os dados do User (name, password, amount, isAdmin) devem ser passados em formato json ou entao da erro
 @app.route('/register', methods = ['POST'])
-def upload():  
+def register():  
     name = request.json['name']
     password = request.json['password']
     amount = request.json['amount']
