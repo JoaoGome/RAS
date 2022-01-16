4070518c42c9660a363e6fe2cbcbaa6ba9f4e92a
João Gomes <jprodriguesgomespoe@gmail.com>
2022-01-16 18:21:43 +0000

    Fechar Aposta

--- Project/BackEnd/Controllers/apostaColetivaController.py
+++ Project/BackEnd/Controllers/apostaColetivaController.py
@@ -1,5 +1,6 @@
 import mysql.connector
 from mysql.connector import Error
+import userController as User
 
 # criar ligação com a base de dados
 def create_server_connection(host_name, user_name, user_password,db_name):
@@ -71,3 +72,44 @@ def fazerAposta(user_id, apostaColetiva_id, resultado, moeda_id, valor):
     '''
 
     return execute_query(connection, query)
+
+def closeAposta (aposta_id, result):
+
+    connection = create_server_connection("localhost", "root", "JprG7654", "mydb")
+    query = f'''
+        SELECT oddsw, oddsd, oddsl FROM apostaColetiva WHERE id={aposta_id};
+    '''
+
+    result1 = read_query(query)
+
+    if (result == 1):
+        odds = [result1][0]
+    elif(result == 0):
+        odds = [result1][1]
+    else:
+        odds = [result1][2]
+
+    query = f'''
+        SELECT * FROM userApostaColectiva WHERE apostaColetiva_id={aposta_id};
+    '''
+
+    result = read_query(query)
+    
+    for x in result:
+        if (x[3] == result):
+            valor = x[5] * odds
+            moeda_id = x[4]
+
+            query = f'''
+                UPDATE userMoeda SET valor={valor} WHERE user_id={x[1]} AND moeda_id={x[4]};
+            '''
+            execute_query(connection, query)
+    
+    query = f'''
+            UPDATE apostaColetiva SET estado={0} WHERE id={aposta_id};
+        '''
+
+    execute_query(connection,query)
+
+
+
